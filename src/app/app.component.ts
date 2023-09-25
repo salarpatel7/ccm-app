7d81f80f0759edd612ee19ebb3c224f3a2393f4a
patel salar <patel.salar@skillassure.com>
2023-09-25 19:46:49 +0530

    ccm-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ccm-app';
-}
+  constructor(private dataService: DataService) {}
+  isUploadhidden=true
+  popUp(){
+    this.isUploadhidden=!this.isUploadhidden
+  }
+  uploadedDocuments: any[] = [];
+
+  openFileInput(): void {
+    // Trigger the file input programmatically
+    const fileInput = document.getElementById('fileInput');
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
+
+  onFileSelected(event: any): void {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      // Handle the selected file here (e.g., upload or process it)
+      console.log(`Selected file: ${selectedFile.name}`);
+      
+      // Add the uploaded document to the array
+      this.uploadedDocuments.push({ id: this.uploadedDocuments.length, filename: selectedFile.name });
+    }
+  }
+  }
+ 
+
